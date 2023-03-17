e97e71c5a5b39a92038a92b67462c442b53818f5
Konrad Gaj <kfocusek@gmail.com>
2023-03-17 22:09:58 +0100

    adding checking correct answer function (prototype)

--- js/app.js
+++ js/app.js
@@ -5,13 +5,14 @@ const answerA = document.getElementById("A");
 const answerB = document.getElementById("B");
 const answerC = document.getElementById("C");
 const answerD = document.getElementById("D");
+const answers = document.getElementsByClassName("answers");
 
 const max = data.length;
 let numberOfElement = numberofQuestion(max);
 
 questionPlace.textContent = data[numberOfElement].question;
 
-getAnswers(numberOfElement, data);
+checkAnswerValue(getAnswers(numberOfElement, data), data);
 
 function numberofQuestion(number) {
   let questionNumber = Math.random() * (0 - number) + number;
@@ -20,8 +21,25 @@ function numberofQuestion(number) {
 }
 
 function getAnswers(numberOfElement, data) {
+  let answerArray = [];
   answerA.textContent = data[numberOfElement].A;
   answerB.textContent = data[numberOfElement].B;
   answerC.textContent = data[numberOfElement].C;
   answerD.textContent = data[numberOfElement].D;
+
+  answerA.value = "A";
+  answerB.value = "B";
+  answerC.value = "C";
+  answerD.value = "D";
+
+  answerArray = [answerA, answerB, answerC, answerD];
+  return answerArray;
+}
+
+function checkAnswerValue(answerArray, data) {
+  for (let i = 0; i < answerArray.length; i++) {
+    if (answerArray[i].value == data[numberOfElement].correct) {
+      console.log(answerArray[i].value);
+    }
+  }
 }
