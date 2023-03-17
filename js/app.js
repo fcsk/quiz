79857ab9907ea686cf9e3374d5019f32c74574e7
Konrad Gaj <kfocusek@gmail.com>
2023-03-17 21:45:08 +0100

    adding answers to correct place on website

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
 "use strict";
 import data from "./questions.json" assert { type: "json" };
 const questionPlace = document.getElementById("questionPlace");
-const max = data.length;
+const answerA = document.getElementById("A");
+const answerB = document.getElementById("B");
+const answerC = document.getElementById("C");
+const answerD = document.getElementById("D");
 
-questionPlace.textContent = data[numberofQuestion(max)].question;
+const max = data.length;
+let numberOfElement = numberofQuestion(max);
+
+questionPlace.textContent = data[numberOfElement].question;
+
+getAnswers(numberOfElement, data);
 
 function numberofQuestion(number) {
   let questionNumber = Math.random() * (0 - number) + number;
   questionNumber = questionNumber.toFixed(0);
   return questionNumber;
+}
+
+function getAnswers(numberOfElement, data) {
+  answerA.textContent = data[numberOfElement].A;
+  answerB.textContent = data[numberOfElement].B;
+  answerC.textContent = data[numberOfElement].C;
+  answerD.textContent = data[numberOfElement].D;
 }
