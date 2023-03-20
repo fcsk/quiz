cd0b63c9f9187be2cdf4ed0d96a966fe4b800b44
Konrad Gaj <kfocusek@gmail.com>
2023-03-20 19:53:38 +0100

    change method of import questions question will be hosted on on an external website. Fixing fetch bug on mozilla and safari

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 "use strict";
-import data from "./questions.json" assert { type: "json" };
 const questionPlace = document.getElementById("questionPlace");
 const answerA = document.getElementById("A");
 const answerB = document.getElementById("B");
@@ -8,54 +7,59 @@ const answerD = document.getElementById("D");
 const answers = document.querySelectorAll(".answers div");
 const buttonNext = document.querySelector(".next-question");
 
-const max = data.length;
-let numberOfElement = numberofQuestion(max);
+async function loadJSON(url) {
+  const res = await fetch(url);
+  return await res.json();
+}
 
-questionPlace.textContent = data[numberOfElement].question;
-getAnswers(numberOfElement, data);
+loadJSON("https://api.npoint.io/ed80ed7e8d638faa9bb6").then((dataJSON) => {
+  const max = dataJSON.length;
+  let numberOfElement = numberofQuestion(max);
 
-function numberofQuestion(number) {
-  let questionNumber = Math.random() * (0 - number) + number;
-  questionNumber = questionNumber.toFixed(0);
+  questionPlace.textContent = dataJSON[numberOfElement].question;
+  getAnswers(numberOfElement, dataJSON);
 
-  if (questionNumber >= data.length) {
-    questionNumber = data.length - 1;
+  function numberofQuestion(number) {
+    let questionNumber = Math.random() * (0 - number) + number;
+    questionNumber = questionNumber.toFixed(0);
+
+    if (questionNumber >= dataJSON.length) {
+      questionNumber = dataJSON.length - 1;
+    }
+
+    return questionNumber;
   }
 
-  return questionNumber;
-}
+  function getAnswers(numberOfElement, data) {
+    answerA.textContent = dataJSON[numberOfElement].A;
+    answerB.textContent = dataJSON[numberOfElement].B;
+    answerC.textContent = dataJSON[numberOfElement].C;
+    answerD.textContent = dataJSON[numberOfElement].D;
 
-function getAnswers(numberOfElement, data) {
-  answerA.textContent = data[numberOfElement].A;
-  answerB.textContent = data[numberOfElement].B;
-  answerC.textContent = data[numberOfElement].C;
-  answerD.textContent = data[numberOfElement].D;
+    answerA.value = "A";
+    answerB.value = "B";
+    answerC.value = "C";
+    answerD.value = "D";
+  }
 
-  answerA.value = "A";
-  answerB.value = "B";
-  answerC.value = "C";
-  answerD.value = "D";
-}
-
-answers.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    if (event.target.value == data[numberOfElement].correct) {
-      console.log("odpowiedź prawidłowa");
-      event.target.classList.add("postivie");
-    } else {
-      console.log("odpowiedź nieprawidłowa");
-      event.target.classList.add("negative");
-    }
-  });
-});
-
-buttonNext?.addEventListener("click", function () {
-  answers.forEach((answer) => {
-    answer.classList.remove("postivie");
-    answer.classList.remove("negative");
+  answers.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      if (event.target.value == dataJSON[numberOfElement].correct) {
+        event.target.classList.add("postivie");
+      } else {
+        event.target.classList.add("negative");
+      }
+    });
   });
 
-  numberOfElement = numberofQuestion(max);
-  questionPlace.textContent = data[numberOfElement].question;
-  getAnswers(numberOfElement, data);
+  buttonNext?.addEventListener("click", function () {
+    answers.forEach((answer) => {
+      answer.classList.remove("postivie");
+      answer.classList.remove("negative");
+    });
+
+    numberOfElement = numberofQuestion(max);
+    questionPlace.textContent = dataJSON[numberOfElement].question;
+    getAnswers(numberOfElement, dataJSON);
+  });
 });
