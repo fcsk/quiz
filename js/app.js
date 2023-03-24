268fd74815363497c2e4ffad4dba119a9cf01e0e
Konrad Gaj <kfocusek@gmail.com>
2023-03-24 20:07:23 +0100

    api migration from my project on github https://github.com/fcsk/question-api

--- js/app.js
+++ js/app.js
@@ -15,86 +15,88 @@ async function loadJSON(url) {
   return await res.json();
 }
 
-loadJSON("https://api.npoint.io/02c931769b9b5499a9c3").then((dataJSON) => {
-  const max = dataJSON.length;
+fetch("https://question-qpi.onrender.com/questions")
+  .then((response) => response.json())
+  .then((data) => data.map((question) => question))
+  .then((questions) => {
+    const max = questions.length;
+    let numberOfElement = numberofQuestion(max);
 
-  let numberOfElement = numberofQuestion(max);
+    getAnswers(numberOfElement, questions);
 
-  questionPlace.textContent = dataJSON[numberOfElement].question;
-  getAnswers(numberOfElement, dataJSON);
+    function numberofQuestion(number) {
+      let questionNumber = Math.random() * (0 - number) + number;
+      questionNumber = questionNumber.toFixed(0);
 
-  function numberofQuestion(number) {
-    let questionNumber = Math.random() * (0 - number) + number;
-    questionNumber = questionNumber.toFixed(0);
+      if (questionNumber >= questions.length) {
+        questionNumber = questions.length - 1;
+      }
 
-    if (questionNumber >= dataJSON.length) {
-      questionNumber = dataJSON.length - 1;
+      return questionNumber;
     }
 
-    return questionNumber;
-  }
+    function getAnswers(numberOfElement, data) {
+      if (scoreCounter == 10 || quizEndCounter == 0) {
+        answerA.style.display = "none";
+        answerB.style.display = "none";
+        answerC.style.display = "none";
+        answerD.style.display = "none";
+        questionPlace.textContent = `Your result is ${scoreCounter} / ${10}. Reload the page`;
+      } else {
+        questionPlace.textContent = data[numberOfElement].question;
+        answerA.textContent = data[numberOfElement].A;
+        answerB.textContent = data[numberOfElement].B;
+        answerC.textContent = data[numberOfElement].C;
+        answerD.textContent = data[numberOfElement].D;
 
-  function getAnswers(numberOfElement, data) {
-    if (scoreCounter == 10 || quizEndCounter == 0) {
-      answerA.style.display = "none";
-      answerB.style.display = "none";
-      answerC.style.display = "none";
-      answerD.style.display = "none";
-      questionPlace.textContent = `Your result is ${scoreCounter} / ${10}. Reload the page`;
-    } else {
-      questionPlace.textContent = dataJSON[numberOfElement].question;
-      answerA.textContent = dataJSON[numberOfElement].A;
-      answerB.textContent = dataJSON[numberOfElement].B;
-      answerC.textContent = dataJSON[numberOfElement].C;
-      answerD.textContent = dataJSON[numberOfElement].D;
-
-      answerA.value = "A";
-      answerB.value = "B";
-      answerC.value = "C";
-      answerD.value = "D";
+        answerA.value = "A";
+        answerB.value = "B";
+        answerC.value = "C";
+        answerD.value = "D";
+      }
     }
-  }
 
-  answers.forEach((element) => {
-    element.addEventListener("click", (event) => {
-      if (event.target.value == dataJSON[numberOfElement].correct) {
-        event.target.classList.add("postivie");
-        if (!element.classList.contains("negative")) {
-          scoreCounter++;
+    answers.forEach((element) => {
+      element.addEventListener("click", (event) => {
+        if (event.target.value == questions[numberOfElement].correct) {
+          event.target.classList.add("postivie");
+          if (!element.classList.contains("negative")) {
+            scoreCounter++;
+            quizEndCounter--;
+
+            setTimeout(function () {
+              numberOfElement = numberofQuestion(max);
+              getAnswers(numberOfElement, questions);
+              event.target.classList.remove("postivie");
+            }, 800);
+          }
+        } else {
+          element.classList.add("negative");
           quizEndCounter--;
-
           setTimeout(function () {
             numberOfElement = numberofQuestion(max);
-            getAnswers(numberOfElement, dataJSON);
-            event.target.classList.remove("postivie");
-          }, 800);
+            getAnswers(numberOfElement, questions);
+            answer.forEach((element) => {
+              element.classList.remove("negative");
+            });
+          }, 700);
         }
-      } else {
-        element.classList.add("negative");
-        quizEndCounter--;
-        setTimeout(function () {
-          numberOfElement = numberofQuestion(max);
-          getAnswers(numberOfElement, dataJSON);
-          answer.forEach((element) => {
-            element.classList.remove("negative");
-          });
-        }, 700);
+      });
+    });
+
+    buttonNext?.addEventListener("click", function () {
+      answers.forEach((answer) => {
+        answer.classList.remove("postivie");
+        answer.classList.remove("negative");
+      });
+      quizEndCounter--;
+      numberOfElement = numberofQuestion(max);
+      questionPlace.textContent = questions[numberOfElement].question;
+      getAnswers(numberOfElement, questions);
+      if (scoreCounter === 10 || quizEndCounter === 0) {
+        scoreCounter = 0;
+        quizEndCounter = 10;
       }
     });
-  });
-
-  buttonNext?.addEventListener("click", function () {
-    answers.forEach((answer) => {
-      answer.classList.remove("postivie");
-      answer.classList.remove("negative");
-    });
-    quizEndCounter--;
-    numberOfElement = numberofQuestion(max);
-    questionPlace.textContent = dataJSON[numberOfElement].question;
-    getAnswers(numberOfElement, dataJSON);
-    if (scoreCounter === 10 || quizEndCounter === 0) {
-      scoreCounter = 0;
-      quizEndCounter = 10;
-    }
-  });
-});
+  })
+  .catch((error) => console.error(error));
